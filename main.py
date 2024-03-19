75b93e2118d8bb14660d281cb47d46b21420b82e
wyy1 <32542363-wyy1@users.noreply.replit.com>
2024-03-19 19:16:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,129 @@
+from flask import Flask, render_template, request
+from flask import jsonify
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from mymodel import FirstModel, SecondModel, ThirdModel
+from joblib import dump, load
+import json
+import pickle
+
+app = Flask(__name__)
+
+data = pd.read_csv('tables/clustering.csv')
+origin_tracts = data['TRACT'].unique().tolist()
+destination_tracts = origin_tracts
+
+age_category = pd.read_csv('tables/pca_values/age_category.csv')
+car_share = pd.read_csv('tables/pca_values/car_share.csv')
+education = pd.read_csv('tables/pca_values/education.csv')
+employment = pd.read_csv('tables/pca_values/employment.csv')
+gender = pd.read_csv('tables/pca_values/gender.csv')
+hhincome_broad = pd.read_csv('tables/pca_values/hhincome_broad.csv')
+license = pd.read_csv('tables/pca_values/license.csv')
+numadults = pd.read_csv('tables/pca_values/numadults.csv').astype(str)
+numchildren = pd.read_csv('tables/pca_values/numchildren.csv').astype(str)
+vehicle_count = pd.read_csv('tables/pca_values/vehicle_count.csv')
+cluster = pd.read_csv("tables/clustering.csv", index_col=0)
+inter = pd.read_csv("tables/inter_cluster.csv", index_col=0)
+outer = pd.read_csv("tables/cross_centroid.csv", index_col=0)
+inter_x = pd.read_csv("tables/inter_x.csv", index_col=0)
+inter_y = pd.read_csv("tables/inter_y.csv", index_col=0)
+outer_x = pd.read_csv("tables/outer_x.csv", index_col=0)
+outer_y = pd.read_csv("tables/outer_y.csv", index_col=0)
+toy = pd.read_csv("tables/second_toy.csv", index_col=0)
+coord = pd.read_csv("tables/coord_xy.csv", index_col=0)
+x = pd.read_csv("tables/x.csv", index_col=0)
+y = pd.read_csv("tables/y.csv", index_col=0)
+w = pd.read_csv("tables/w.csv", index_col=0)
+
+
+#first_model = FirstModel(cluster)
+#first_model.train(x,y,w)
+#with open('model/firstmodel.pkl', 'wb') as f:
+#  pickle.dump(first_model, f)
+
+#second_model = SecondModel(toy, coord)
+#with open('model/secondmodel.pkl', 'wb') as f:
+#  pickle.dump(second_model, f)
+
+#third_model = ThirdModel(cluster, inter, outer)
+#third_model.train(inter_x, inter_y, outer_x, outer_y)
+
+with open('model/firstmodel.pkl', 'rb') as f:
+  first_model = pickle.load(f)
+
+with open('model/secondmodel.pkl', 'rb') as f:
+  second_model = pickle.load(f)
+
+with open('model/thirdmodel.pkl', 'rb') as f:
+  third_model = pickle.load(f)
+
+
+# Define routes
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    volume = 0
+    prediction_results = ['None','None','None','None','None','None']
+    if request.method == 'POST':
+        # Get form data
+        features = [
+            request.form['vehicle_count'],
+            request.form['hhincome_broad'],
+            request.form['car_share'],
+            float(request.form['numadults']),
+            float(request.form['numchildren']),
+            request.form['age_category'],
+            request.form['gender'],
+            request.form['employment'],
+            request.form['education'],
+            request.form['license'],
+            float(request.form['origin_census_tract']),
+            float(request.form['destination_census_tract'])
+        ]
+
+        volume = first_model.predict(features[:11])
+
+        origin_coord = second_model.predict(float(request.form['origin_census_tract']))[0].tolist()[0]
+
+        destination_coord = second_model.predict(float(request.form['origin_census_tract']))[1].tolist()[0]
+
+        destination = second_model.predict(float(request.form['origin_census_tract']))[2]
+
+        # Make prediction using the loaded model)
+        prediction = third_model.predict(features)
+        prediction[prediction<0] = 0
+        prediction[5] = prediction[6]
+        prediction = prediction[0:6]
+
+        prediction = np.round(prediction/sum(prediction),2)
+
+        # Assuming prediction is a list of 6 floats
+        prediction_results = [str(result) for result in prediction]
+
+        return jsonify(html=render_template('prediction_results.html',
+     volume=volume,
+     prediction_results=prediction_results),
+  origin_coord=origin_coord,
+  destination_coord=destination_coord)
+        
+
+    # Render the index template with no prediction results
+    return render_template('index.html', 
+                           age_category=age_category['age_category'].unique().tolist(),
+                           car_share=car_share['car_share'].unique().tolist(),
+                           education=education['education'].unique().tolist(),
+                           employment=employment['employment'].unique().tolist(),
+                           gender=gender['gender'].unique().tolist(),
+                           hhincome_broad=hhincome_broad['hhincome_broad'].unique().tolist(),
+                           license=license['license'].unique().tolist(),
+                           numadults=numadults['numadults'].unique().tolist(),
+                           numchildren=numchildren['numchildren'].unique().tolist(),
+                           vehicle_count=vehicle_count['vehicle_count'].unique().tolist(),
+                           origin_tracts=origin_tracts,
+                           destination_tracts=destination_tracts,
+                           volume = volume,
+                           prediction_results=prediction_results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ mymodel.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import numpy as np
+from sklearn.neighbors import KernelDensity
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LinearRegression
+
+class FirstModel():
+  def __init__(self, cluster):
+      cluster['o_tract10'] = cluster['TRACT'].astype(float)
+      self.cluster = cluster
+      self.pls = PLSRegression(n_components=10)
+      self.encoder = OneHotEncoder()
+      self.linear = LinearRegression()
+      self.knn = KNeighborsRegressor()
+
+  def train(self, x, y, w):
+    x['o_tract10'] = x[['o_tract10']].merge(self.cluster[['o_tract10', 'Cluster']], on='o_tract10', how="left")['Cluster'].fillna(-1)
+    x,y,w = x.iloc[:,1:], y.iloc[:,1:].values, w.iloc[:,1:].values
+
+    x = self.encoder.fit_transform(x.values).toarray()
+    x, _ = self.pls.fit_transform(x, y)
+    self.linear.fit(x,y,sample_weight = w.ravel())
+
+  def predict(self, features):
+    features[-1] = self.cluster[self.cluster['o_tract10']==features[-1]]['Cluster'].fillna(-1).values[0]
+    x = self.pls.transform(self.encoder.transform([features]).toarray())
+    return self.linear.predict(x).astype(int)[0]
+
+    
+
+class SecondModel():
+  def __init__(self, toy, coord):
+    self.toy = toy
+    self.coord = coord
+
+  def predict(self, origin):
+    destination  = self.toy[self.toy['o_tract10']==origin]['d_tract10'].values[0]
+    o_coord = self.coord[self.coord['TRACT']==origin][["centroid_y", "centroid_x"]].values
+    d_coord = self.coord[self.coord['TRACT']==destination][["centroid_y", "centroid_x"]].values
+    return o_coord, d_coord, destination
+
+    
+
+class ThirdModel():
+    def __init__(self, cluster, inter, outer):
+        self.cluster = cluster
+        self.inter = inter
+        self.outer = outer
+        self.inter_model = PLSRegression(n_components=10)
+        self.outer_model = PLSRegression(n_components=10)
+        self.encoder_x = OneHotEncoder()
+        self.encoder_y = OneHotEncoder()
+
+    def train(self, inter_x, inter_y, outer_x, outer_y):
+      self.encoder_x.fit_transform(pd.concat([inter_x.iloc[:,:10], outer_x.iloc[:,:10]], axis=0).values)
+      inter_x_soc = pd.DataFrame(self.encoder_x.transform(inter_x.iloc[:,:10].values).toarray())
+      inter_x = pd.concat([inter_x_soc, inter_x.iloc[:,10:]], axis=1)
+      outer_x_soc = pd.DataFrame(self.encoder_x.transform(outer_x.iloc[:,:10].values).toarray()).reset_index().drop(columns="index")
+      outer_x = outer_x.reset_index().drop(columns="index")
+      outer_x = pd.concat([outer_x_soc, outer_x.iloc[:,10:]], axis=1)
+      y = pd.concat([inter_y, outer_y], axis=0).reset_index()
+      y = y.pivot_table(index=y.index,columns="main_mode",values="weight").fillna(0)
+      inter_len = len(inter_y)
+      inter_y = y.iloc[:inter_len, :]
+      outer_y = y.iloc[inter_len:, :]
+      self.inter_model.fit(inter_x.values, inter_y.values)
+      self.outer_model.fit(outer_x.values, outer_y.values)
+
+    def predict(self, features):
+      val = self.encoder_x.transform([features[:-2]])[0].toarray()[0].tolist()
+      if features[-2] == features[-1]:
+        att = self.cluster.merge(self.inter, on='Cluster')
+        value = att[att['TRACT'] == features[-1]].iloc[:,2:].values.tolist()[0]
+        for ele in value:
+          val.append(ele)
+        result = self.inter_model.predict([val])
+      else:
+        att = self.cluster.merge(self.outer, on='TRACT', how='left')
+        ct = features[-2]
+        value_o = att[att['TRACT']==ct][['centroid_x','centroid_y']].values[0]
+        ct = features[-1]
+        value_d = att[att['TRACT']==ct][['centroid_x','centroid_y']].values[0]
+        value = np.sqrt(sum((value_o-value_d)**2))
+        val.append(value)
+        result = self.outer_model.predict([val])
+      return result[0]
